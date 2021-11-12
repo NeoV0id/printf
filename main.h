854ce1e6e48068b59e0ea2530a1116ca33305d49
Anna78990 <takasuanna1990@gmail.com>
2021-11-12 13:45:02 +0000

    for indentation

--- main.h
+++ main.h
@@ -11,7 +11,7 @@ typedef struct printing
 void print_numbers(va_list, int *);
 void print_numbers_rec(unsigned int a, int *p);
 void put_char(va_list, int *);
-void print_string(va_list , int *);
+void print_string(va_list, int *);
 void print_percent(va_list , int *);
 int _putchar (char c);
 void (*get_sp_func(char ch))(va_list, int *);

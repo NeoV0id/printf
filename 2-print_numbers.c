37d37d28c629bdb1911f96f8ad9c4f945b1c010c
LucasD <3242@holbertonschool.com>
2021-11-12 13:46:54 +0100

    print number for %i and %d

--- 2-print_numbers.c
+++ 2-print_numbers.c
@@ -1,40 +1,38 @@
 #include "main.h"
-
 /**
  * print_numbers - check if the number is negative or positive
  * @ap: va_list
  * @p: pointer to count
  * Return: nothing
  */
-
 void print_numbers(va_list ap, int *p)
 {
 	int num;
-	unsigned int un_num;
-
-	num = va_arg(ap, int);
-	if (num < 0)
+	unsigned int un_num;	num = va_arg(ap, int);
+	if (num == 0)
 	{
-		un_num = (unsigned int)(num * -1);
-		_putchar('-');
+		_putchar('0');
 		*p += 1;
 	}
 	else
 	{
-		un_num = (unsigned int)num;
+		if (num < 0)
+		{
+			un_num = (unsigned int)(num * -1);
+			_putchar('-');
+			*p += 1;
+		}
+		else
+			un_num = (unsigned int)num;
+		print_numbers_rec(un_num, p);
+		*p -= 1;
 	}
-	print_numbers_rec(un_num, p);
-	*p -= 1;
-}
-
-/**
+}/**
  * print_numbers_rec - print the numbers
  * @a: number to print
  * @p: pointer to count
  * Return: nothing
- */
-
-void print_numbers_rec(unsigned int a, int *p)
+ */void print_numbers_rec(unsigned int a, int *p)
 {
 	*p += 1;
 	if (a >= 1)
